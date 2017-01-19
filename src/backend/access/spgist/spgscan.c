1a1b7db30e442608a6b2a64297f6d806a72669a7
Nikita Glukhov <n.gluhov@postgrespro.ru>
2017-01-19 15:57:36 +0300

    Remove SpGistSearchItemIsHeap() macro

--- src/backend/access/spgist/spgscan.c
+++ src/backend/access/spgist/spgscan.c
@@ -50,10 +50,10 @@ pairingheap_SpGistSearchItem_cmp(const pairingheap_node *a,
 			return (sa->distances[i] < sb->distances[i]) ? 1 : -1;
 	}
 
-	/* Heap items go before inner pages, to ensure a depth-first search */
-	if (SpGistSearchItemIsHeap(*sa) && !SpGistSearchItemIsHeap(*sb))
+	/* Leaf items go before inner pages, to ensure a depth-first search */
+	if (sa->isLeaf && !sb->isLeaf)
 		return 1;
-	if (!SpGistSearchItemIsHeap(*sa) && SpGistSearchItemIsHeap(*sb))
+	if (!sa->isLeaf && sb->isLeaf)
 		return -1;
 
 	return 0;
@@ -693,7 +693,7 @@ redirect:
 		/* Check for interrupts, just in case of infinite loop */
 		CHECK_FOR_INTERRUPTS();
 
-		if (SpGistSearchItemIsHeap(*item))
+		if (item->isLeaf)
 		{
 			/* We store heap items in the queue only in case of ordered search */
 			Assert(so->numberOfOrderBys > 0);
--- src/include/access/spgist_private.h
+++ src/include/access/spgist_private.h
@@ -148,8 +148,6 @@ typedef struct SpGistSearchItem
 	double		distances[FLEXIBLE_ARRAY_MEMBER];
 } SpGistSearchItem;
 
-#define SpGistSearchItemIsHeap(item)	((item).isLeaf)
-
 #define SizeOfSpGistSearchItem(n_distances) \
 	(offsetof(SpGistSearchItem, distances) + sizeof(double) * (n_distances))
 
